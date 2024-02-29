c20362fb88e0910f19cce786f9c380f463666845
Mark Smith <mark@lenovo.us.com>
2024-02-29 14:44:25 -0800

    Minor changes in slack bot

--- system/slack/integration_chat.py
+++ system/slack/integration_chat.py
@@ -4,8 +4,8 @@ const options = {
   text: "Message from slack bot!!",
 };
 
-slack = "T06L0LNHA58/B06LX4U4J4B/alSW0feBStQwRsjT1pFrKJ1j"
-axios.post('https://hooks.slack.com/services/ + slack', JSON.stringify(options))
+slack = "XXXXXXXXXXXXXX"
+axios.post('SLACK WEBHOOK URL', JSON.stringify(options))
 .then((response) => {
   console.log('SUCCEEDED: Sent slack webhook: \n', response.data);
   resolve(response.data);
